f435ad4b6783ec390e07106245dcde792c2e8077
mrazizi <mreza.azizi74@gmail.com>
2018-09-03 17:40:45 +0430

    added certificate pinning to Retrofit

--- app/src/main/java/org/neshan/sample/starter/network/RetrofitClientInstance.java
+++ app/src/main/java/org/neshan/sample/starter/network/RetrofitClientInstance.java
@@ -1,5 +1,7 @@
 package org.neshan.sample.starter.network;
 
+import okhttp3.CertificatePinner;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,10 +11,22 @@ public class RetrofitClientInstance {
     private static final String BASE_URL = "https://api.neshan.org/v1/";
 
     public static Retrofit getRetrofitInstance() {
+        // creating a CertificatePinner object and adding public key of neshan.org to it
+        CertificatePinner certPinner = new CertificatePinner.Builder()
+                .add("*.neshan.org",
+                        "sha256/Cyg7e5STKgZCwdABdPZlqO5lQWSE0KbWr624HoIUuUc=")
+                .build();
+
+        // adding the created certPinner to OkHttpClient
+        OkHttpClient client = new OkHttpClient.Builder()
+                .certificatePinner(certPinner)
+                .build();
+
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit;

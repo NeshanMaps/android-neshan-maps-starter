126aa586ed224fbaf0ec903ebcb91a0de7a8b09f
rezaiyan <alirezaiyann@gmail.com>
2020-04-18 19:24:15 +0430

    update reverse usage

--- app/src/main/java/org/neshan/sample/starter/network/RetrofitClientInstance.java
+++ app/src/main/java/org/neshan/sample/starter/network/RetrofitClientInstance.java
@@ -1,5 +1,7 @@
 package org.neshan.sample.starter.network;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,6 +19,7 @@ public class RetrofitClientInstance {
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(client)
                     .build();
